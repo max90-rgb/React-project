ab814f576dc4b98db4d4ae0f96ff86226b685d85
candidato2 <sysadmin+2@exabytesrl.it>
2023-07-31 16:27:39 +0200

    test directory

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react' 
 
-function App() {
+/* const App = () => {
+  const [counter, setCounter] = useState(0)
+  return  <div>
+    <h1>Ciao sono app</h1>
+    <button onClick={
+     () => setCounter(counter-1)         decremento del counter
+    }>- 1</button>
+    {counter}
+  </div> 
+} */
+
+const App = () => {
+
+  const [inputValue, setInputValue] = useState('');
+  const [savedValues, setSavedValues] = useState([]);
+
+  const handleAddClick = () => {
+    if (inputValue.trim() !== '') {
+      setSavedValues([...savedValues, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const renderSavedValues = () => {
+    return savedValues.map((value, index) => (
+      <p key={index}>{value}</p>
+    ));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input id="input" type="text" placeholder="Enter engagements" value={inputValue} 
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleAddClick}>Add</button>
+
+      {renderSavedValues()}
     </div>
   );
-}
+};
 
 export default App;
+
